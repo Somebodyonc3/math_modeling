c677ccf84fbb17c8f4e665fc0977e2d23e03e1e6
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2021-10-25 16:03:14 +0000

    lkdflv

---
+++ p2_t4_d.py
@@ -0,0 +1,7 @@
+# Вывести на экран, из каких простых множителей состоит введенное с клавиатуры натуральное число.
+a = int(input())
+factors = []
+for t in range(1, a + 1):
+  if a % t == 0:
+    factors.append(t)
+print(factors)
