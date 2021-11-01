ea92c987c224051a64ccaea2fb192f845a3429c1
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2021-11-01 14:51:16 +0000

    nvng

--- Дополнительный уровень/p2_t4_d.py
+++
@@ -1,8 +0,0 @@
-# Вывести на экран, из каких простых множителей состоит введенное с клавиатуры натуральное число.
-
-a = int(input())
-factors = []
-for t in range(1, a + 1):
-  if a % t == 0:
-    factors.append(t)
-print(factors)
