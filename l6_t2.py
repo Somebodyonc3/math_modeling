490fb0cdddb5f1895a9726380a180fa53d186439
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2021-12-06 16:56:58 +0000

    neat

--- l6_t2.py
+++ l6_t2.py
@@ -26,15 +26,15 @@ def parabola(a, b, c, k, Xa, Xb, N,  title = 'Парабола и Гипербо
 
 def hyperbola(k, Xa, Xb, title = 'Гипербола'):
   x1 = numpy.linspace(Xa,Xb, N)
+  x1 = numpy.around(x1, decimals = 4)
+  x1 = numpy.ma.masked_where(abs(x1) == 0.0, x1)
   y1 = k / x1
-  
-  x2 = x1[y1 == 0]
-  y2 = y1[y1 == 0]
-  
-  y3 = numpy.ma.masked_where(abs(y1) <= 0.0001, y1)
-  x3 = numpy.ma.masked_where(abs(x1) <= 0.0001, x1)
+  y3 = numpy.ma.masked_where(abs(x1) == 0.0, y1)
 
-  plt.plot(x3, y3, label = 'Гипербола, Ура!')
+  
+
+
+  plt.plot(x1, y3, label = 'Гипербола, Ура!')
   plt.xlabel('X')
   plt.ylabel('Y')
   plt.title(title)
@@ -42,4 +42,4 @@ def hyperbola(k, Xa, Xb, title = 'Гипербола'):
   plt.show()
 
 hyperbola(k, Xa, Xb) 
-#разкомментируй чтоб увидеть Гиперболу и закоментируй параболу
+#разкомментируй чтоб увидеть Гиперболу и закоментируй параболу и наоборот 
