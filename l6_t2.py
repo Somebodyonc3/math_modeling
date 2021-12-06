dc24a8045153b6228b52b2197c1c8ed709f25fba
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2021-12-06 16:40:35 +0000

    not much

--- l6_t2.py
+++ l6_t2.py
@@ -2,21 +2,44 @@
 import matplotlib.pyplot as plt
 import numpy
 
-Xa = int(input())
+
+Xa = int(input("Отрицательное пжпжпжпжп:"))
 Xb = int(input())
 N = int(input())
-a = int(input())   
+a = int(input()) 
 b = int(input())
 c = int(input())
+k = int(input('Неравное нулю:'))
 
-def parabola_and_giperbola(a, b, c, Xa, Xb, N,  title = 'Паробола и гипербола'):
+def parabola(a, b, c, k, Xa, Xb, N,  title = 'Парабола и Гипербола'):
   x = numpy.linspace(Xa, Xb, N)
-  
   y = a * x ** 2 + b * x + c
-  y1 = 1 / x
 
-  plt.plot(x, y)
-  plt.plot(x, y1)
+  plt.plot(x, y, label = 'Парабола, Finally!')
+  plt.xlabel('X')
+  plt.ylabel('Y')
+  plt.title(title)
+  plt.legend()
   plt.show()
 
-parabola_and_giperbola(a, b, c, Xa, Xb, N)
+#parabola(a, b, c, k, Xa, Xb, N)
+
+def hyperbola(k, Xa, Xb, title = 'Гипербола'):
+  x1 = numpy.linspace(Xa,Xb, N)
+  y1 = k / x1
+  
+  x2 = x1[y1 == 0]
+  y2 = y1[y1 == 0]
+  
+  y3 = numpy.ma.masked_where(abs(y1) <= 0.0001, y1)
+  x3 = numpy.ma.masked_where(abs(x1) <= 0.0001, x1)
+
+  plt.plot(x3, y3, label = 'Гипербола, Ура!')
+  plt.xlabel('X')
+  plt.ylabel('Y')
+  plt.title(title)
+  plt.legend()
+  plt.show()
+
+hyperbola(k, Xa, Xb) 
+#разкомментируй чтоб увидеть Гиперболу и закоментируй параболу
