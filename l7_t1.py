9d721578512ff2f96b29c951dfd4578d13573de8
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2021-12-13 17:15:51 +0000

    Not all only 4 is left

--- l7_t1.py
+++ l7_t1.py
@@ -3,16 +3,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-t = np.arange(-2 * np.pi, 2  * np.pi, 0.1) 
-R = 3
+t = np.arange(-2*np.pi, 2*np.pi, 0.1)
+R = 2
+
 def cykloida(t, R):
 
-  x = R * np.cos(t)
-  y = R * np.sin(t)
+  x = R * (t - np.sin(t) ** 3)
+  y = R * (1 - np.cos(t) ** 3)
 
-  plt.plot(x, y, ls = '--', lw = 3)
+  plt.plot(x, y, lw = 3)
 
   plt.axis('equal')
 
   plt.show()
+
 cykloida(t, R)
+
+# График Астроиды
+
+def astroida(t, R):
+
+  x = R * np.cos(t) ** 3
+  y = R * np.sin(t) ** 3
+
+  plt.plot(x, y, lw = 3)
+
+  plt.axis('equal')
+
+  plt.show()
+
+astroida(t, R)
