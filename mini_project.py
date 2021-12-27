53bbedb1199d6d4827c5c0e71d66e51c1e1cc2b5
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2021-12-27 05:37:28 +0000

    уау

---
+++ mini_project.py
@@ -0,0 +1,36 @@
+import numpy
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+#Бабочка
+
+fig, ax = plt.subplots()
+line, = plt.plot([], [], '--', color = 'green', label = 'line')
+ 
+edge = 10
+ 
+plt.axis('equal')
+ 
+ax.set_xlim(-edge, edge)
+ax.set_ylim(-edge, edge)
+ 
+x = []
+y = []
+ 
+def animate(frame):
+    t = frame
+    d = numpy.exp(numpy.cos(t)) - 2 * numpy.cos(4 * t) + (numpy.sin(t / 12)) ** 5
+    
+    xn = numpy.sin(t) * d
+    yn = numpy.cos(t) * d
+ 
+    x.append(xn)
+    y.append(yn)
+    line.set_data(x, y)
+ 
+ani = animation.FuncAnimation(fig,
+                        animate,
+                        frames = numpy.arange(0, 100, 0.1),
+                        interval = 30)
+ 
+ani.save('my_project.gif')
