2972b2d3ec14d09eb2d5f65ca6315a4d9761d4c9
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2022-01-09 18:22:28 +0000

    neat

--- deafault/l5_t1.py
+++
@@ -1,9 +0,0 @@
-#Написать функцию, которая вычисляет среднее арифметическое элементов одномерного массива, переданного ей в качестве аргумента.
-import numpy as np
-
-lst = np.random.randint(0, 1000, 300)
-b = np.array(lst)
-
-def averadge_x(b):
-  return np.mean(b)
-print(averadge_x(b))
