f24d98009dcd3f3f6d8fb7b45b5c11c8c798a208
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2022-02-07 15:21:04 +0000

    TO DO

---
+++ l8_d_t2.py
@@ -0,0 +1,25 @@
+import numpy
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+k = 340 * 10 ** (-8)
+Esun = 1367 
+t = numpy.arange(0, 24, 0.1)
+
+def victoria(S, t):
+	
+	a = numpy.pi /12 * (t - 12)
+	cosin = numpy.cos(a)
+	if cosin < 0:
+		cosin = 0
+	R = numpy.sqrt(S / numpy.pi)
+	E = S * k * Esun * cosin
+	dsdt = R * E
+	return dsdt
+
+S_0 = 1600
+
+solve = odeint(victoria, S_0, t)
+
+plt.plot(t, solve[:,0])
+plt.show()
