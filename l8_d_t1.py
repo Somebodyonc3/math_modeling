c0925c35504366c3268b796764d158b7a7d16853
Somebodyonc3 <91481770+Somebodyonc3@users.noreply.github.com>
2022-02-14 18:44:32 +0000

    TODO

--- l8_d_t1.py
+++ l8_d_t1.py
@@ -6,23 +6,23 @@ import numpy
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-m1 = int(input('Введите массу метеорита'))
+m1 = 10 ** 19
 h0 = 44000
 R = 6371
 M =  5.972 * 10 ** 24
 g = 9.88
-h = numpy.arange(h0, R, 1)
+h = numpy.arange(h0, R, 0.1)
 
-G = (m1 * g * (h0 + R) ** 2) / m1 * M
+G = (m1 * g * (h0 - R) ** 2) / m1 * M
 
 def meteor(v, h):
 	
-	v = numpy.sqrt(2 * ((m1 * g * (h0 + R) ** 2) / m1 * M) * h)
+	v = numpy.sqrt(2 * G * h)
 	dvdh = v / h
 	
 	return dvdh 
 
 solve = odeint(meteor, G, h)
 
-plt.plot(t, solve[:,0])
+plt.plot(h, solve[:,0])
 plt.show()
